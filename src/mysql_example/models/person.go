4c5d444a4ea0a1f00f602450fe5d8bf167afb32c
Terry Moreland <tsmoreland@live.com>
2022-09-16 00:02:51 +0100

    - some simple methods for Person struct

--- src/mysql_example/models/person.go
+++ src/mysql_example/models/person.go
@@ -1,7 +1,17 @@
 package models
 
+import "fmt"
+
 type Person struct {
 	Id        int64
 	Firstname string
 	Lastname  string
+}
+
+func (p Person) FullName() string {
+	return fmt.Sprintf("%v %v", p.Firstname, p.Lastname)
+}
+
+func (p Person) SortingName() string {
+	return fmt.Sprintf("%v, %v", p.Lastname, p.Firstname)
 }
