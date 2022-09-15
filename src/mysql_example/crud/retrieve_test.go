3fe18002f27551f3aea0fc0821408c9c67e4d5c6
Terry Moreland <tsmoreland@live.com>
2022-09-16 00:18:51 +0100

    - additional unit test

--- src/mysql_example/crud/retrieve_test.go
+++ src/mysql_example/crud/retrieve_test.go
@@ -1,1 +1,37 @@
 package crud
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"log"
+	"testing"
+)
+
+func Test_FindByIdShouldCallQueryWhenDatabaseAvailabile(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	rows := sqlmock.
+		NewRows([]string{"id", "first_name", "last_name"}).
+		AddRow("1", "John", "Smith")
+
+	mock.
+		ExpectQuery("SELECT id, first_name, last_name FROM people WHERE").
+		WillReturnRows(rows)
+
+	p, err := FindById(db, 1)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if p == nil {
+		t.Fatalf("Query not executed")
+	}
+}
