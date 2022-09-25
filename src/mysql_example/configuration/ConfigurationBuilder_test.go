a5e946f466afe68444fd3c48ca3df8152d9bc785
Terry Moreland <tsmoreland@live.com>
2022-09-25 13:39:04 +0100

    - add another test for ConfigurationBuilder

--- src/mysql_example/configuration/ConfigurationBuilder_test.go
+++ src/mysql_example/configuration/ConfigurationBuilder_test.go
@@ -14,5 +14,30 @@ func Test_AddJsonFileShouldSetErrorWhenFileNotFound(t *testing.T) {
 	if err == nil {
 		t.Fatalf("error is nil, expected failure due to file not found")
 	}
+}
 
+func Test_AddJsonShouldNotSetErrorWhenFileExistsAndIsValid(t *testing.T) {
+	filename, err := arrangeTestJsonFile(true, true, t)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c, err := NewBuilder().AddJsonFile(filename).Build()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if c == nil {
+		t.Fatalf("configuration is nil")
+	}
+
+	err = checkIfConfigurationIsExpected(c, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if c.SecretId() != "" {
+		t.Fatalf("secret was returned with configuration")
+	}
 }
