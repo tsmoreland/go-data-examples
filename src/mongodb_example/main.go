e61fe1d8a1e33c48be3385adb7bdf6c24934ce9e
Terry Moreland <tsmoreland@live.com>
2022-10-12 23:59:45 +0100

    - main

--- src/mongodb_example/main.go
+++ src/mongodb_example/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/tsmoreland/go-data-examples/mongodb_example/configuration"
+	"github.com/tsmoreland/go-data-examples/mongodb_example/crud"
+	"github.com/tsmoreland/go-data-examples/mongodb_example/model"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -39,4 +43,41 @@ func main() {
 		panic(err)
 	}
 
+	collection := client.Database("go-example").Collection("People")
+
+	person := model.Person{FirstName: "John", LastName: "Smith"}
+	result, err := crud.Add(collection, context.TODO(), person)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id := result.InsertedID
+	fmt.Println("Last inserted id ", id)
+
+	filter := bson.D{{"lastName", "Smith"}}
+	match, err := crud.FindOneByFilter(collection, context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Found: %v", match.FirstName)
+
+	update := bson.D{{"$set",
+		bson.D{
+			{"firstName", "James"},
+		},
+	}}
+
+	matches, modified, err := crud.Update(collection, context.TODO(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Update: %v matched, %v modified", matches, modified)
+
+	deleteCount, err := crud.Delete(collection, context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Deleted %v records", deleteCount)
 }
