15048790e678f9c6f3f294e4154e99600a017fe1
Terry Moreland <terry.s.moreland@gmail.com>
2022-11-08 18:53:24 +0000

    establish connection to database with super secure credentials

--- src/gorm-example/main.go
+++ src/gorm-example/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	"log"
+)
 
+func main() {
+	db, err := gorm.Open("postgres", "user=admin password=p@55w0rd!, dbname=gormExample, sslmode=disable")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Print(err)
+		}
+	}()
+
+	dbase := db.DB()
+	defer func() {
+		if err := dbase.Close(); err != nil {
+			log.Print(err)
+		}
+	}()
+
+	if err := dbase.Ping(); err != nil {
+		panic(err.Error())
+	}
+
+	log.Print("Connected to database.")
 }
