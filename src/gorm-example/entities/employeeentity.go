b5c2d39244cb322bce22b5880d412d3bf21f3bcc
Terry Moreland <tsmoreland@live.com>
2022-11-10 23:07:51 +0000

    - update schema for employee entity to prevent null first/last name and default job category to 1

--- src/gorm-example/entities/employeeentity.go
+++ src/gorm-example/entities/employeeentity.go
@@ -2,9 +2,9 @@ package entities
 
 type EmployeeEntity struct {
 	ID          uint
-	FirstName   string `sql:"type:VARCHAR(100)"`
-	LastName    string `sql:"size:100"`
-	JobCategory int32
+	FirstName   string `sql:"type:VARCHAR(100);not null"`
+	LastName    string `sql:"size:100;not null"`
+	JobCategory int32  `sql:DEFAULT:1`
 }
 
 func (e EmployeeEntity) TableName() string {
