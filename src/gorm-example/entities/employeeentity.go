f017d8fdfc96f0bdbcd0c84fa813bf14283ec8f1
Terry Moreland <tsmoreland@live.com>
2022-11-10 23:23:17 +0000

    rename JobCategoryValueObject to JobCategoryLink

--- src/gorm-example/entities/employeeentity.go
+++ src/gorm-example/entities/employeeentity.go
@@ -1,12 +1,25 @@
 package entities
 
+import "github.com/jinzhu/gorm"
+
 type EmployeeEntity struct {
 	ID          uint
-	FirstName   string `sql:"type:VARCHAR(100);not null"`
-	LastName    string `sql:"size:100;not null"`
-	JobCategory int32  `sql:"not null;DEFAULT:'1'"`
+	FirstName   string          `sql:"type:VARCHAR(100);not null" gorm:"column:first_name"`
+	LastName    string          `sql:"size:100;not null"`
+	JobCategory JobCategoryLink `gorm:"embedded"`
+}
+
+type JobCategoryLink struct {
+	JobCategoryID   uint   `sql:"not null;DEFAULT:'1'"`
+	JobCategoryName string `sql:"size:100;not null;DEFAULT:'Engineer'"`
 }
 
 func (e EmployeeEntity) TableName() string {
 	return "employees"
+}
+
+func CreateEmployeeTable(db *gorm.DB) {
+	db.
+		CreateTable(&EmployeeEntity{}).
+		Model(&EmployeeEntity{}).AddIndex("idx_employee_job_category_name", "job_category_name")
 }
