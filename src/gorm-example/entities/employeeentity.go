4b11b08ba034ed4e18ece947f3a98e0a0a187e81
Terry Moreland <tsmoreland@live.com>
2022-11-10 23:03:26 +0000

    - add employee entity copied from model, set table name by method

--- src/gorm-example/entities/employeeentity.go
+++ src/gorm-example/entities/employeeentity.go
@@ -1,1 +1,12 @@
 package entities
+
+type EmployeeEntity struct {
+	ID          uint
+	FirstName   string `sql:"type:VARCHAR(100)"`
+	LastName    string `sql:"size:100"`
+	JobCategory int32
+}
+
+func (e EmployeeEntity) TableName() string {
+	return "employees"
+}
