a93fc44ef4ba4e7b1c4ec65a41764c73caf19283
Terry Moreland <tsmoreland@live.com>
2022-11-10 23:09:44 +0000

    update job category to be non-null and correct applying default (SQL settings)

--- src/gorm-example/entities/employeeentity.go
+++ src/gorm-example/entities/employeeentity.go
@@ -4,7 +4,7 @@ type EmployeeEntity struct {
 	ID          uint
 	FirstName   string `sql:"type:VARCHAR(100);not null"`
 	LastName    string `sql:"size:100;not null"`
-	JobCategory int32  `sql:DEFAULT:1`
+	JobCategory int32  `sql:"not null;DEFAULT:'1'"`
 }
 
 func (e EmployeeEntity) TableName() string {
