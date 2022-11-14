a90c0b448d98ce727aa49bb42b4e6c2e928b17e6
Terry Moreland <terry.s.moreland@gmail.com>
2022-11-14 14:10:12 +0000

    add get all employees using Find

--- src/gorm-example/examples/querysets.go
+++ src/gorm-example/examples/querysets.go
@@ -1,1 +1,12 @@
 package examples
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/tsmoreland/go-data-examples/src/gormexample/entities"
+)
+
+func GetAllEmployees(db *gorm.DB) []entities.Employee {
+	var employees []entities.Employee
+	db.Debug().Find(&employees)
+	return employees
+}
