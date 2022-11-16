94903d0b936f01d0a928fe1928082eb403f858ff
Terry Moreland <tsmoreland@live.com>
2022-11-16 22:30:56 +0000

    simple where clause example

--- src/gorm-example/examples/querysets.go
+++ src/gorm-example/examples/querysets.go
@@ -1,1 +1,15 @@
 package examples
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/tsmoreland/go-data-examples/src/gormexample/entities"
+)
+
+func FindByName(db *gorm.DB, firstName string, lastName string) []entities.Employee {
+	var employees []entities.Employee
+	db.
+		Debug().
+		Where("first_name = ? AND last_name = ?", firstName, lastName).
+		Find(&employees)
+	return employees
+}
