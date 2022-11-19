f20c061d34ec0a5ba0bc33ae2d57f9ffb48292eb
Terry Moreland <terry.s.moreland@gmail.com>
2022-11-19 00:31:34 +0000

    adding example of Attrs to populate values when record is not found

---
+++ src/gorm-example/examples/queryattributes.go
@@ -0,0 +1,16 @@
+package examples
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/tsmoreland/go-data-examples/src/gormexample/entities"
+)
+
+func firstByLastNameOrInvalid(db *gorm.DB, lastName string) *entities.Employee {
+	var employee entities.Employee
+	db.
+		Debug().
+		Where("last_name", lastName).
+		Attrs("id", 0).
+		FirstOrInit(&employee)
+	return &employee
+}
