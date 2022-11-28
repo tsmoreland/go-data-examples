eb9dd5475f17039f594f6f08ab976b54dc515fd6
Terry Moreland <tsmoreland@live.com>
2022-11-28 00:54:44 +0000

    updated implementaton of SqliteRepository to implement Employee and Department Repository

--- src/sqlite_example/infrastructure/sqliterepository.go
+++ src/sqlite_example/infrastructure/sqliterepository.go
@@ -2,8 +2,11 @@ package infrastructure
 
 import (
 	"database/sql"
+	"errors"
+	"github.com/mattn/go-sqlite3"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/tsmoreland/go-data-examplessqlite_example/domain"
+	"github.com/tsmoreland/go-data-examplessqlite_example/shared"
 	"log"
 	"os"
 )
@@ -27,22 +30,96 @@ func NewSqliteRepository(filename string) (domain.Repository, error) {
 }
 
 //goland:noinspection ALL
-func (r SqliteRepository) Migrate() error {
+func (r *SqliteRepository) Migrate() error {
 	query := `
-	CREATE TABLE IF NOT EXISTS People (
-	  id INT AUTO_INCREMENT PRIMARY KEY,
-	  first_name varchar(100),
-	  last_name varchar(100)
-	)`
-	result, err := r.db.Exec(query)
+	CREATE TABLE IF NOT EXISTS Department (
+	   id INT AUTO_INCREMENT PRIMARY KEY,
+	   name varchar(50),
+	)
+	`
+	_, err := r.db.Exec(query)
 	if err != nil {
 		return err
 	}
 
-	_ = result
+	query = `
+	CREATE TABLE IF NOT EXISTS Employees (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+	  	first_name varchar(100),
+	  	last_name varchar(100),
+	    is_developer int,
+	    department_id int not null,
+	    CONSTRAINT fk_departments
+	    	FOREIGN KEY (department_id)
+	    	REFERENCES departments(department_id)
+	)`
+	_, err = r.db.Exec(query)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (r SqliteRepository) Close() error {
+func (r *SqliteRepository) Close() error {
 	return r.db.Close()
+}
+
+func (r *SqliteRepository) Employees() domain.EmployeeRepository {
+	return r
+}
+func (r *SqliteRepository) Departments() domain.DepartmentsRepository {
+	return r
+}
+
+func (r *SqliteRepository) CreateEmployee(employee domain.Employee) (*domain.Employee, error) {
+	command := "INSERT INTO Employees (id, first_name, last_name, is_developer, department_id) VALUES (?, ?, ?, ?)"
+	isDeveloper := 0
+	if employee.IsDeveloper {
+		isDeveloper = 1
+	}
+
+	res, err := r.db.Exec(command, employee.FirstName, employee.LastName, isDeveloper, employee.Department.Id)
+	if err != nil {
+		var sqliteErr sqlite3.Error
+		if errors.As(err, &sqliteErr) && errors.Is(sqliteErr.ExtendedCode, sqlite3.ErrConstraintUnique) {
+			return nil, shared.ErrDuplicate
+		}
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	employee.Id = id
+
+	return &employee, nil
+}
+func (r *SqliteRepository) FindEmployeeById(id int) (*domain.Employee, error) {
+	return nil, shared.ErrNotImplemented
+}
+func (r *SqliteRepository) FindAllEmployees(pageNumber int, pageSize int) ([]domain.Employee, error) {
+	return nil, shared.ErrNotImplemented
+}
+func (r *SqliteRepository) UpdateEmployee(employee domain.Employee) error {
+	return shared.ErrNotImplemented
+}
+func (r *SqliteRepository) DeleteEmployee(employee domain.Employee) error {
+	return shared.ErrNotImplemented
+}
+
+func (r *SqliteRepository) CreateDepartment(department domain.Department) (*domain.Department, error) {
+	return nil, shared.ErrNotImplemented
+}
+func (r *SqliteRepository) FindDepartmentById(id int) (*domain.Department, error) {
+	return nil, shared.ErrNotImplemented
+}
+func (r *SqliteRepository) FindAllDepartments(pageNumber int, pageSize int, includeEmployees bool) ([]domain.Department, error) {
+	return nil, shared.ErrNotImplemented
+}
+func (r *SqliteRepository) UpdateDepartment(department domain.Department) error {
+	return shared.ErrNotImplemented
+}
+func (r *SqliteRepository) DeleteDepartment(department domain.Department) error {
+	return shared.ErrNotImplemented
 }
