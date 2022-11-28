ed84a1d6429ed418ad534e822caa173655ef32ff
Terry Moreland <tsmoreland@live.com>
2022-11-28 22:45:34 +0000

    new error(s)

--- src/sqlite_example/shared/errors.go
+++ src/sqlite_example/shared/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrNotImplemented = errors.New("not implemented")
 	ErrDuplicate      = errors.New("item already exists")
+	ErrNotFound       = errors.New("no match found")
 )
