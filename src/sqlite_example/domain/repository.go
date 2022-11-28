121fce51f8f9143f775bff52217d06edadbd4b43
Terry Moreland <tsmoreland@live.com>
2022-11-28 00:31:13 +0000

    add employee and department repository, exposed as methods on Repository

--- src/sqlite_example/domain/repository.go
+++ src/sqlite_example/domain/repository.go
@@ -3,4 +3,22 @@ package domain
 type Repository interface {
 	Migrate() error
 	Close() error
+
+	Employees() EmployeeRepository
+	Departments() EmployeeRepository
+}
+
+type EmployeeRepository interface {
+	Create(employee Employee) (*Employee, error)
+	FindById(id int) (*Employee, error)
+	FindAll(pageNumber int, pageSize int) ([]Employee, error)
+	Update(employee Employee) error
+	Delete(employee Employee) error
+}
+type DepartmentRepository interface {
+	Create(department Department) (*Department, error)
+	FindById(id int) (*Department, error)
+	FindAll(pageNumber int, pageSize int) ([]Department, error)
+	Update(department Department) error
+	Delete(department Department) error
 }
