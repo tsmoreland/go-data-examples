a1b778e2c03c895644fc176cbfe296153f50e1ce
Terry Moreland <tsmoreland@live.com>
2022-11-29 00:35:22 +0000

    new errors

--- src/sqlite_example/shared/errors.go
+++ src/sqlite_example/shared/errors.go
@@ -3,7 +3,8 @@ package shared
 import "errors"
 
 var (
-	ErrNotImplemented = errors.New("not implemented")
-	ErrDuplicate      = errors.New("item already exists")
-	ErrNotFound       = errors.New("no match found")
+	ErrNotImplemented  = errors.New("not implemented")
+	ErrDuplicate       = errors.New("item already exists")
+	ErrNotFound        = errors.New("no match found")
+	ErrInvalidArgument = errors.New("one or more arguments is invalid")
 )
