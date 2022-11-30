67562586076c85b163c8d3449494927cec8c0fb7
Terry Moreland <tsmoreland@live.com>
2022-11-30 22:49:50 +0000

    implement custom String() method for department and employee

--- src/sqlite_example/domain/models.go
+++ src/sqlite_example/domain/models.go
@@ -1,5 +1,7 @@
 package domain
 
+import "fmt"
+
 type Employee struct {
 	Id           int64
 	FirstName    string
@@ -9,8 +11,20 @@ type Employee struct {
 	Department   *Department
 }
 
+func (e Employee) String() string {
+	if e.Department != nil {
+		return fmt.Sprintf("%v: %v %v (%v) in %v", e.Id, e.FirstName, e.LastName, e.IsDeveloper, e.Department)
+	} else {
+		return fmt.Sprintf("%v: %v %v (%v)", e.Id, e.FirstName, e.LastName, e.IsDeveloper)
+	}
+}
+
 type Department struct {
 	Id        int64
 	Name      string
 	Employees []Employee
+}
+
+func (d Department) String() string {
+	return fmt.Sprintf("(%v: %v)", d.Id, d.Name)
 }
