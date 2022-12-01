c731e9518e17eaabfd50d8423b881c3054d0bd5c
Terry Moreland <tsmoreland@live.com>
2022-12-01 20:33:16 +0000

    change update methods to upsert

--- src/sqlite_example/domain/repository.go
+++ src/sqlite_example/domain/repository.go
@@ -13,13 +13,13 @@ type EmployeeRepository interface {
 	CreateEmployee(employee Employee) (*Employee, error)
 	FindEmployeeById(id int) (*Employee, error)
 	FindAllEmployees(pageNumber int, pageSize int) ([]Employee, error)
-	UpdateEmployee(employee Employee) error
+	UpsertEmployee(employee Employee) (*Employee, error)
 	DeleteEmployee(employee Employee) error
 }
 type DepartmentRepository interface {
 	CreateDepartment(department Department) (*Department, error)
 	FindDepartmentById(id int, includeEmployees bool) (*Department, error)
 	FindAllDepartments(pageNumber int, pageSize int, includeEmployees bool) ([]Department, error)
-	UpdateDepartment(department Department) error
+	UpsertDepartment(department Department) (*Department, error)
 	DeleteDepartment(department Department) error
 }
