a81ff566f3666ad23b1a12414b4ae687fd2384e2
Terry Moreland <tsmoreland@live.com>
2022-12-01 20:31:43 +0000

    remove unused error

--- src/sqlite_example/shared/errors.go
+++ src/sqlite_example/shared/errors.go
@@ -8,5 +8,4 @@ var (
 	ErrNotFound        = errors.New("no match found")
 	ErrInvalidArgument = errors.New("one or more arguments is invalid")
 	ErrDeleteFailed    = errors.New("delete failed, no matching rows found")
-	ErrUnableToUpdate  = errors.New("unable to update, record does not exist")
 )
