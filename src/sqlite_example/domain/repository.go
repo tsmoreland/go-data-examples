de978aee8d7ac8274ee4e53080ad7778be45774b
Terry Moreland <tsmoreland@live.com>
2022-12-01 21:55:08 +0000

    correct methods taking id to take it as int64

--- src/sqlite_example/domain/repository.go
+++ src/sqlite_example/domain/repository.go
@@ -11,14 +11,15 @@ type Repository interface {
 
 type EmployeeRepository interface {
 	CreateEmployee(employee Employee) (*Employee, error)
-	FindEmployeeById(id int) (*Employee, error)
+	FindEmployeeById(id int64) (*Employee, error)
 	FindAllEmployees(pageNumber int, pageSize int) ([]Employee, error)
+	FindAllEmployeesWithDepartmentId(departmentId int64, pageNumber int, pageSize int) ([]Employee, error)
 	UpsertEmployee(employee Employee) (*Employee, error)
 	DeleteEmployee(employee Employee) error
 }
 type DepartmentRepository interface {
 	CreateDepartment(department Department) (*Department, error)
-	FindDepartmentById(id int, includeEmployees bool) (*Department, error)
+	FindDepartmentById(id int64, includeEmployees bool) (*Department, error)
 	FindAllDepartments(pageNumber int, pageSize int, includeEmployees bool) ([]Department, error)
 	UpsertDepartment(department Department) (*Department, error)
 	DeleteDepartment(department Department) error
